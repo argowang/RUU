3a93a28e693a717e42ad86360353ee3bfba88f83
MrDoggie <wangjunyi@hotmail.com>
2018-11-29 13:30:09 -0500

    modify keylogger

--- auditControlCenter.sh
+++ auditControlCenter.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 function startKeyLogger() {
+    keyloggerHeartBeat &
+    echo $! > save_keylog_heartbeat_pid.txt
     keylogger ./log/keyStroke.log &
     # make sure the process is terminated later
     echo $! > save_keylog_pid.txt
+}
+
+function keyloggerHeartBeat() {
+    while true;
+    do
+        echo "\n***10s KeyLogger Milestone***" >> log/keyStroke.log
+        sleep 10 &
+        keylog_sleep_pid=$!
+        echo $keylog_sleep_pid > save_keylog_sleep_pid.txt
+        wait $keylog_sleep_pid
+    done
 }
 
 function runProcessAudit() {
@@ -21,12 +34,16 @@ function runChromeTabAudit() {
 
 function cleanUp() {
     echo "Ctrl-C caught...performing clean up"
+    kill -9 `cat save_keylog_heartbeat_pid.txt`
+    kill -9 `cat save_keylog_sleep_pid.txt`
     kill -9 `cat save_window_audit_pid.txt`
     kill -9 `cat save_chrome_audit_pid.txt`
     kill -9 `cat save_keylog_pid.txt`
     kill -9 `cat save_sleep_pid.txt`
     rm save_window_audit_pid.txt
     rm save_chrome_audit_pid.txt
+    rm save_keylog_heartbeat_pid.txt
+    rm save_keylog_sleep_pid.txt
     rm save_keylog_pid.txt
     rm save_sleep_pid.txt
     exit 2
